1b29e0413e831046168fd51d8e098f8700fb0b8e
bistiana29 <bistianafn29@gmail.com>
2025-05-28 07:31:33 +0700

    delete connect_database.py

--- backend/app/connect_database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/iahVision"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
